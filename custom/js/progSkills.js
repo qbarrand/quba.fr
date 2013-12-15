7d07ab497d6f2b84f3fb5913239b3c1f9ca47cae
Quentin Barrand <quentin@quentinbarrand.com>
2013-12-15 23:40:12 +0100

    Improved stars on skills Worked on OSM for travels

---
+++ custom/js/progSkills.js
@@ -0,0 +1,54 @@
+// Call to function with anonymous callback
+loadProgSkills(function(response) {
+
+    data = JSON.parse(response);
+
+    var table;
+    table = "<table>";
+
+    for(var i in data)
+    {
+      table += "<tr><td><strong>" + data[i].name + "</strong></td><td></td></tr>";
+
+      for(var j in data[i].languages)
+      {
+        table += "<tr>";
+        table += "<td>" + data[i].languages[j].name + "</td>"
+
+        table += "<td>";
+
+        var k = 0;
+
+        for(; k < data[i].languages[j].skill; k++)
+          table += "<i class=\"fa fa-star\"> </i>";
+
+        for(; k < 5; k++)
+          table += "<i class=\"fa fa-star-o\"> </i>";
+
+        table += "</td>";
+        table += "</tr>";
+
+      }
+    }
+
+    table += "</table>";
+
+    document.getElementById("tableProgSkills").innerHTML = table;
+});
+
+
+function loadProgSkills(callback) {  
+
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', 'custom/data/prog_skills.json', true);
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+           
+            // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback(xobj.responseText);
+     
+        }
+    }
+    xobj.send(null);
+}
