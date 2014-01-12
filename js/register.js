d067b7cf375e97749fb8c3997da89c29c7a1ad63
Quentin Barrand <quentin@quentinbarrand.com>
2014-01-12 17:51:57 +0100

    Map is now partially working

---
+++ js/register.js
@@ -0,0 +1,9 @@
+/* OpenLayers */
+$("#modal_travels").on('shown.bs.modal', function() {
+	loadTravelsMap();
+});
+
+/* Load Hackgyver modal */
+$("#modal_hackgyver").on('shown.bs.modal', function() {
+	loadHackGyverModal();
+});
