15cb134616014c866be9ac5792ccdc7e0fe8cac4
Quentin Barrand <quentin@quba.fr>
2014-03-04 22:19:34 +0100

    No Google Fonts anymore !

--- js/register.js
+++
@@ -1,9 +0,0 @@
-/* OpenLayers */
-$("#modal_travels").on('shown.bs.modal', function() {
-	loadTravelsMap();
-});
-
-/* Load Hackgyver modal */
-$("#modal_hackgyver").on('shown.bs.modal', function() {
-	loadHackGyverModal();
-});
