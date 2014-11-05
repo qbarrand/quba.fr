f7f4780a8d2dbc1dbb9def57e33a162db479b62a
Quentin Barrand <quentin@quba.fr>
2014-11-05 10:17:11 +0100

    Modified the website to use one simpler mailing system in mail.php.

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+
+$to = "quentin@quba.fr";
+
+$from_name = $_POST['name'];
+$from_email = $_POST['email'];
+$body = $_POST['message'];
+
+$subject = "New message from " . $from_name . " (" . $from_email . ")";
+$headers = "From: " . $from_email . "\r\n";
+
+$response = array();
+
+if(mail($to, $subject, $body, $headers)) {
+	$response["status"] = true;
+}
+else {
+	$response["status"] = false;
+}
+
+header("Content-type:application/json");
+echo(json_encode($response));
