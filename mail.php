d295857e9382994ae30f4ec58110acaf657b5fb7
Quentin Barrand <quentin@quba.fr>
2016-03-23 15:25:35 +0100

    Added a local file where the mails are stored after they are sent using mail().

--- mail.php
+++ mail.php
@@ -13,4 +13,17 @@ $headers = "From: " . $from_email . "\r\n";
 
 $response["status"] = mail($to, $subject, $body, $headers);
 
+// Send the response to the client
 echo(json_encode($response));
+
+fastcgi_finish_request();
+
+$response["client_data"] = $_POST;
+
+// Also save it locally in case there was an issue with the MTA
+$mails = fopen('mails.json', "a");
+
+fwrite($mails, json_encode($response));
+fwrite($mails, "\n");
+
+fclose($mails);
