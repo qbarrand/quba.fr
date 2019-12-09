d42b5d30655ce44b75201d93a05244564a13721f
Quentin Barrand <quentin@quba.fr>
2019-12-09 08:48:31 +0100

    use the Accept header to specify the image format.

--- assets/js/main.0.js
+++ assets/js/main.0.js
@@ -98,10 +98,12 @@ async function printRandomBackground(parent, imageName, constraint) {
         webpPromiseRan = true;
     }
 
+    let accept = ['image/jpeg'];
+
     console.log(`webp supported: ${webpSupported}`)
 
     if (webpSupported) {
-        url += '&format=webp'
+        accept = ['image/webp'].concat(accept);
     }
 
     let imageData = cache.get(url);
@@ -111,7 +113,10 @@ async function printRandomBackground(parent, imageName, constraint) {
     } else {
         console.log('Fetching ' + url);
 
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: new Headers({'Accept': accept.join(',')})
+        });
+
         const blob = await response.blob();
 
         const objUrl = URL.createObjectURL(blob);
@@ -150,7 +155,7 @@ async function printRandomBackground(parent, imageName, constraint) {
             !function(){function t(t){this.el=t;for(var n=t.className.replace(/^\s+|\s+$/g,"").split(/\s+/),i=0;i<n.length;i++)e.call(this,n[i])}function n(t,n,i){Object.defineProperty?Object.defineProperty(t,n,{get:i}):t.__defineGetter__(n,i)}if(!("undefined"==typeof window.Element||"classList"in document.documentElement)){var i=Array.prototype,e=i.push,s=i.splice,o=i.join;t.prototype={add:function(t){this.contains(t)||(e.call(this,t),this.el.className=this.toString())},contains:function(t){return-1!=this.el.className.indexOf(t)},item:function(t){return this[t]||null},remove:function(t){if(this.contains(t)){for(var n=0;n<this.length&&this[n]!=t;n++);s.call(this,n,1),this.el.className=this.toString()}},toString:function(){return o.call(this," ")},toggle:function(t){return this.contains(t)?this.remove(t):this.add(t),this.contains(t)}},window.DOMTokenList=t,n(Element.prototype,"classList",function(){return new t(this)})}}();
 
         // canUse
-            window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
+            // window.canUse=function(p){if(!window._canUse)window._canUse=document.createElement("div");var e=window._canUse.style,up=p.charAt(0).toUpperCase()+p.slice(1);return p in e||"Moz"+up in e||"Webkit"+up in e||"O"+up in e||"ms"+up in e};
 
         // window.addEventListener
             (function(){if("addEventListener"in window)return;window.addEventListener=function(type,f){window.attachEvent("on"+type,f)}})();
