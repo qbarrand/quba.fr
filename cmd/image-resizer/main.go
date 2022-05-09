e96464528696ccc5294b82a4ba4aa0df5f854498
Quentin Barrand <quentin@quba.fr>
2022-05-10 00:37:27 +0200

    Remove an unused assignment.

--- cmd/image-resizer/main.go
+++ cmd/image-resizer/main.go
@@ -266,7 +266,7 @@ func main() {
 	go func() {
 		defer wg.Done()
 
-		for baseName, _ := range meta {
+		for baseName := range meta {
 			fullPath := filepath.Join(imgInDir, baseName)
 
 			b, err := os.ReadFile(fullPath)
