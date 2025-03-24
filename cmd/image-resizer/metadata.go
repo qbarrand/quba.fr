78f758308483ba3afc548bbd632d4817c07d8121
Quentin Barrand <quentin@quba.dev>
2025-03-25 00:26:31 +0100

    ci: use the golangci-lint action
    Fix linter errors in the code.

--- cmd/image-resizer/metadata.go
+++ cmd/image-resizer/metadata.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"os"
 )
@@ -22,12 +23,16 @@ func Read(r io.Reader) (Metadata, error) {
 	return m, json.NewDecoder(r).Decode(&m)
 }
 
-func ReadFromFile(name string) (Metadata, error) {
+func ReadFromFile(name string) (m Metadata, err error) {
 	fd, err := os.Open(name)
 	if err != nil {
 		return nil, err
 	}
-	defer fd.Close()
+	defer func() {
+		if cerr := fd.Close(); cerr != nil && err == nil {
+			err = fmt.Errorf("could not close the file: %v", cerr)
+		}
+	}()
 
 	return Read(fd)
 }
